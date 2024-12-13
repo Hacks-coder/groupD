cbec6dc04f29820098bd60b42b7e3c75952ea467
Hacks-coder <ajheartcry@gmail.com>
2024-12-13 13:59:26 -0800

    Add files via upload

---
+++ groupD/data.js
@@ -0,0 +1,22 @@
+// data.js
+const persons = [
+  {
+    id: 1,
+    name: "Person One",
+    image: "./photos/person1.jpg",
+    details: "Details about Person One.",
+  },
+  {
+    id: 2,
+    name: "Person Two",
+    image: "./photos/person1.jpg",
+    details: "Details about Person Two.",
+  },
+  {
+    id: 3,
+    name: "Person Three",
+    image: "./photos/person1.jpg",
+    details: "Details about Person Three.",
+  },
+  // Add more persons as needed
+];
